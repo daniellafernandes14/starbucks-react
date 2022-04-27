62feab9f7ef6bce9b59acac914aa3108baa5d567
Daniella Fernandes <fernandesdaniella@hotmail.com>
2022-04-27 12:02:41 +0100

    Updated header element for Header componenet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
 const Header = () => {
   return (
-    <nav role='navigation' className='navbar'>
-      <div className='left-links'>
-        <a href="/" className='logo'><img src='../images/logo.png' alt='logo' /></a>
-        <a href='https://google.com'>MENU</a>
-        <a href='https://google.com'>REWARDS</a>
-        <a href='https://google.com'>GIFT CARDS</a>
-      </div>
-      <div className='right-links'>
-        <a href='https://google.com'>📍 Find a store</a>
-        <a href='https://google.com' className='nav-btn-white'>Sign in</a>
-        <a href='https://google.com' className='nav-btn-black' style={{color: 'white'}}>Join now</a>
-      </div>
-    </nav>
+    <header>
+      <nav role='navigation' className='navbar'>
+        <div className='left-links'>
+          <a href="/" className='logo'><img src='../images/logo.png' alt='logo' /></a>
+          <a href='https://google.com'>MENU</a>
+          <a href='https://google.com'>REWARDS</a>
+          <a href='https://google.com'>GIFT CARDS</a>
+        </div>
+        <div className='right-links'>
+          <a href='https://google.com'>📍 Find a store</a>
+          <a href='https://google.com' className='nav-btn-white'>Sign in</a>
+          <a href='https://google.com' className='nav-btn-black' style={{color: 'white'}}>Join now</a>
+        </div>
+      </nav>
+    </header>
   )
 }
 
